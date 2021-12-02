c76b3e0e2296af40fca0185c9f6b057c2d6618d4
mgmavely <mgmavely@gmail.com>
2021-12-01 22:34:10 -0500

    Modified script to grab live data from binance websocket and store in sqlite database

--- main.py
+++ main.py
@@ -1,50 +1,37 @@
 import os
+import asyncio
 import pandas as pd
-from binance import Client
-import matplotlib.pyplot as plt
 import sqlalchemy
-from binance import BinanceSocketManager
+from binance import BinanceSocketManager, AsyncClient
 
 api_key = os.environ.get("API_KEY")
 secret_key = os.environ.get("SECRET_KEY")
 
 
-def getMinuteData(symbol, interval, lookback):
-    frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback + " min ago UTC-05:00"))
-    frame = frame.iloc[:, :6]
-    frame.columns = ["Time", "Open", "High", "Low", "Close", "Volume"]
-    frame = frame.set_index("Time")
-    frame.index = pd.to_datetime(frame.index, unit="ms")
-    frame = frame.astype(float)
-    return frame
-
-
-def strategyTest(symbol, qty, entered=False):
-    df = getMinuteData(symbol, '1m', '30')
-    cumulret = (df.Open.pct_change() + 1).cumprod() - 1
-    print(cumulret)
-    if not entered:
-        if cumulret[-1] < -0.002:
-            order = client.create_order(symbol=symbol, side="BUY", type="MARKET", quantity=qty)
-            print(order)
-            entered = True
-        else:
-            print("No Trade Executed.")
-    if entered:
+async def main():
+    client = await AsyncClient.create(api_key=api_key, api_secret=secret_key)
+    bsm = BinanceSocketManager(client)
+    socket = bsm.trade_socket('BTCUSDT')
+    async with socket as tscm:
         while True:
-            df = getMinuteData(symbol, '1m', '30')
-            sincebuy = df.loc[df.index > pd.to_datetime(order['transactTime'], unit="ms")]
-            if len(sincebuy) > 0:
-                sincebuyret = (sincebuy.Open.pct_change() + 1).cumprod() - 1
-                if sincebuy[-1] > 0.0015 or sincebuyret[-1] < -0.0015:
-                    order = client.create_order(symbol=symbol, side="SELL", type="MARKET", quantity=qty)
-                    print(order)
-                    break
+            res = await tscm.recv()
+            df = createDF(res)
+            df.to_sql('BTCUSDT', engine, if_exists='append', index=False)
+            print(df)
+
+    await client.close_connection()
+
+
+def createDF(msg):
+    df = pd.DataFrame([msg])
+    df = df.loc[:, ['s', 'E', 'p']]
+    df.columns = ['Symbol', 'Time', 'Price']
+    df.Price = df.Price.astype(float)
+    df.Time = pd.to_datetime(df.Time, unit='ms')
+    return df
 
 
 if __name__ == '__main__':
-    client = Client(api_key=api_key, api_secret=secret_key)
-    client_account = client.get_account()
-    strategyTest('BTCUSDT', 0.001)
-    # plt.plot(btc_history)
-    # plt.show()
+    engine = sqlalchemy.create_engine('sqlite:///BTCUSDTstream.db')
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
